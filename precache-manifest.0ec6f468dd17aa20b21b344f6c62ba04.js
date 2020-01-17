022e2398632e2ee1ffc9bf5f6e45c884dcdb242c
Sihle Socishe <sihlesocishe@gmail.com>
2020-01-17 14:30:11 +0200

    Updates

---
+++ precache-manifest.0ec6f468dd17aa20b21b344f6c62ba04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e6bde3474d1ced28384b49743e65758",
+    "url": "/pokegame/index.html"
+  },
+  {
+    "revision": "0657e0bee89a854fa741",
+    "url": "/pokegame/static/css/main.87b3647a.chunk.css"
+  },
+  {
+    "revision": "e20db773506337e4079f",
+    "url": "/pokegame/static/js/2.ba672487.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pokegame/static/js/2.ba672487.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0657e0bee89a854fa741",
+    "url": "/pokegame/static/js/main.219e592e.chunk.js"
+  },
+  {
+    "revision": "bc21af16913e2a8864aa",
+    "url": "/pokegame/static/js/runtime-main.26d31c5e.js"
+  }
+]);
